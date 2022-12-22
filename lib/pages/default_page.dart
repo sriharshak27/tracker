351d092ba6f2a73ae1ffe7cec84a3adf26dbb4bd
sriharshak27 <sriharshakocherla27@gmail.com>
2022-12-22 12:04:34 -0500

    change

--- lib/pages/default_page.dart
+++ lib/pages/default_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class DefaultPage extends StatefulWidget {
@@ -8,8 +9,9 @@ class DefaultPage extends StatefulWidget {
   @override
   State<DefaultPage> createState() => _DefaultPageState();
 }
-
 class _DefaultPageState extends State<DefaultPage> {
+  final userid = FirebaseAuth.instance.currentUser?.uid;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
